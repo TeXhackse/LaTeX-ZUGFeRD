2911b5ea8c3b2cad12c42ddf6246d297c9b70e72
Marei (peiTeX) <marei@peitex.de>
2024-09-11 22:31:32 +0200

    CTAN release

--- build.lua
+++ build.lua
@@ -1,9 +1,24 @@
 #!/usr/bin/env texlua
+
+--[[
+	Build script for the LaTeX-ZUGFeRD project
+	Copyright (C) 2024 Marei Peischl <marei@peitex.de>
+
+	This file is part of the build system of the zugferd package.
+
+	It may be distributed and/or modified under the conditions of the
+	LaTeX Project Public License (LPPL), either version 1.3c of this
+	license or (at your option) any later version. The latest version
+	of this license is in the file
+
+	https://www.latex-project.org/lppl.txt
+]]
  
 module="zugferd"
+packageversion= "0.8"
+packagedate   = "2024-09-11"
 
 sourcefiles={"*.dtx","*.ins", "*.sty"}
-
 demofiles={"*.tex"}
 typesetdemofiles={"*.tex"}
 
@@ -29,3 +44,25 @@ function update_tag(file, content, tagname, tagdate)
 	end
 	return content
 end
+
+github_base_url="https://github.com/TeXhackse/LaTeX-ZUGFeRD"
+gitlab_base_url="https://gitlab.com/islandoftex/texmf/"..module
+
+ctanpkg=module
+uploadconfig = {
+    author = "Marei Peischl",
+    uploader = "Marei Peischl",
+    description="Create ZUGFeRD, X-Rechnung or Faktur-X invoices with LaTeX",
+    pkg = ctanpkg,
+    version = packageversion .. " " .. packagedate,
+    license = "lppl1.3c",
+    summary = "ZUGFeRD and Faktur-X invoicing using LaTeX",
+    ctanPath = "/macros/latex/contrib/" .. ctanpkg,
+    repository = {gitlab_base_url..".git",github_base_url..".git"},
+    bugtracker = {gitlab_base_url.."/issues",github_base_url.."/issues"},
+    support = {gitlab_base_url.."/issues",github_base_url.."/issues"},
+    announcement_file = "ctan.ann",
+    update = true,
+    topic   = "invoice",
+    note_file = "ctan.note"
+}
