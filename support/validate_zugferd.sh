d9162ccb0a7b6b0ac8abcd97253f4aa8b5aa0ad7
Marei (peiTeX) <marei@peitex.de>
2024-10-22 20:39:33 +0200

    change file ending detectinon for vlaidation script to allow to be used in l3build check

--- support/validate_zugferd.sh
+++ support/validate_zugferd.sh
@@ -7,7 +7,19 @@ if [ ! -f "$mustang_jar" ]; then
 	wget -O "$mustang_jar" "https://github.com/ZUGFeRD/mustangproject/releases/download/core-$mustang_version/Mustang-CLI-$mustang_version.jar"
 fi
 
-for f in "$@"
+validate_zugferd () {
+	for f in "$@"
+	do
+		if [[ -f "$f" ]]; then
+			java -Xmx1G -Dfile.encoding=UTF-8 -jar $mustang_jar --no-notices --action validate --source  "$f"
+		fi
+	done
+}
+
+for file in "$@"
 do
-	java -Xmx1G -Dfile.encoding=UTF-8 -jar "$mustang_jar" --no-notices --action validate --source "$f"
+	# search corresponding xml or pdf file
+	for variant in "$file" "$file.pdf" "$file.xmpl" "${file}_zugferd.xml"; do
+		validate_zugferd "$variant"
+	done
 done
