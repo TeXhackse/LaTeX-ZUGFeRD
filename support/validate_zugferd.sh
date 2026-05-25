61f55d382a0e14af7d4abe503be1c1f6dc4a4156
Marei (peiTeX) <marei@peitex.de>
2026-05-25 19:19:46 +0200

    update mustang

--- support/validate_zugferd.sh
+++ support/validate_zugferd.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-mustang_version="2.21.0"
+mustang_version="2.23.0"
 mustang_jar="Mustang-CLI_$mustang_version.jar"
 
 # add scriptdir as a variable to also check support/ for the jar as it's the case on CI pipeline runs
